89175796b44d3d221b8958dc0e31f2650ba659c0
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-10-14 21:00:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/spotify/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setElevation(0);
+    }
+}
---
+++ app/src/main/java/com/example/spotify/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.login_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
